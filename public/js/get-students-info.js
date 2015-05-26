bac6cce0a5786ae098b545a133ff9e4ff7e1dc5c
zc1415926 <zc1415926@126.com>
2015-05-26 12:49:32 +0800

    save standard refresh students table

--- public/js/get-students-info.js
+++ public/js/get-students-info.js
@@ -112,6 +112,7 @@ function onSubmitStandardClick(){
             console.log('standard');
             console.log(data);
             //refreshStudentsTable();
+            getStudentsFromClass();
 
         },
         'json'
